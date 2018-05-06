18b968d809245c32e643dbb4b4eb53e4e137904e
price.of.the.flight@hotmail.com <hptor_i27Q2>
2018-05-06 13:09:56 +0200

    add whole scope of images to the project

--- main.py
+++ main.py
@@ -15,24 +15,37 @@ def main():
     pygame.display.set_caption("2018 on pygame")
     background = Surface(DISPLAY)
 
+    # data
+    data_array = [[0, 0, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]]
+
     # draw cells
     entities = pygame.sprite.Group()
     x = y = 0
     for i in range(4):
         for j in range(4):
-            print(x, y)
-            cell = Cell(x, y)
+            print(x, y, data_array[i][j])
+            cell = Cell(x, y, data_array[i][j])
             entities.add(cell)
             x += SPACE_FOR_ONE_CELL
         y += SPACE_FOR_ONE_CELL
         x = 0
 
     while 1:  # main loop
+        action = False
 
         # input handling
         for e in pygame.event.get():
             if e.type == QUIT:
                 raise SystemExit("QUIT")
+
+        while action:
+
+            # draw updated cells here
+
+            action = False
 
         screen.blit(background, (0, 0))
         entities.draw(screen)
