6152eabce232c97b7c7049e8ec43369214ca912b
price.of.the.flight@hotmail.com <hptor_i27Q2>
2018-05-06 13:23:21 +0200

    Generate new value (2 or 4) in empty cells

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from pygame import *
 
@@ -40,16 +41,45 @@ def main():
         for e in pygame.event.get():
             if e.type == QUIT:
                 raise SystemExit("QUIT")
+            if e.type == KEYUP and e.key == K_RIGHT:
+                action = True
 
         while action:
 
             # draw updated cells here
+            new_value = generate_input_value()
+            x_coor, y_coor = generate_coordinates_for_input(data_array)  # coordinated for new value
+            data_array[x_coor][y_coor] = new_value
+
+            x = y = 0
+            for i in range(4):
+                for j in range(4):
+                    print(x, y, data_array[i][j])
+                    cell = Cell(x, y, data_array[i][j])
+                    entities.add(cell)
+                    x += SPACE_FOR_ONE_CELL
+                y += SPACE_FOR_ONE_CELL
+                x = 0
 
             action = False
 
         screen.blit(background, (0, 0))
         entities.draw(screen)
         pygame.display.update()
+
+
+def generate_input_value():
+    input_values = [2, 4]
+    return random.choice(input_values)
+
+
+def generate_coordinates_for_input(input_array):
+    empty_coordinates = []
+    for i in range(4):
+        for j in range(4):
+            if input_array[i][j] == 0:
+                empty_coordinates.append([i, j])
+    return random.choice(empty_coordinates)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,31 +2,36 @@ import random
 
 
 def main():
-    data_array = [[0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0]]
-    print(data_array)
-    for i in range(5):
-        a = generate_input_value()
-        print(a)
-        b, c = generate_coordinates_for_input(data_array)
-        print(b, c)
-        data_array[b][c] = a
-        print(data_array)
+    a, b = [0, 1]
+    print(a)
+    print(b)
 
-def generate_input_value():
-    input_values = [2, 4]
-    return random.choice(input_values)
 
-def generate_coordinates_for_input(input_array):
-    empty_coordinates = []
-    for i in range(len(input_array)):
-        for j in range(len(input_array[i])):
-            if input_array[i][j] == 0:
-                empty_coordinates.append([i, j])
-
-    return random.choice(empty_coordinates)
+#     data_array = [[0, 0, 0, 0],
+#              [0, 0, 0, 0],
+#              [0, 0, 0, 0],
+#              [0, 0, 0, 0]]
+#     print(data_array)
+#     for i in range(5):
+#         a = generate_input_value()
+#         print(a)
+#         b, c = generate_coordinates_for_input(data_array)
+#         print(b, c)
+#         data_array[b][c] = a
+#         print(data_array)
+#
+# def generate_input_value():
+#     input_values = [2, 4]
+#     return random.choice(input_values)
+#
+# def generate_coordinates_for_input(input_array):
+#     empty_coordinates = []
+#     for i in range(len(input_array)):
+#         for j in range(len(input_array[i])):
+#             if input_array[i][j] == 0:
+#                 empty_coordinates.append([i, j])
+#
+#     return random.choice(empty_coordinates)
 
 if __name__ == "__main__":
     main()
